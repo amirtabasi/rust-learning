29a4b43a470f0a035a8366d2f6d0a549afaae1c7
AmirTabasi <AmirTabasi21@gmail.com>
2025-07-25 21:24:26 +0330

    variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -18,11 +18,10 @@ fn main() {
     const TWO: u32 = 1 + 1;
     println!("two is {TWO}");
 
-
     let x = 5;
 
     let x = x + 1; // Shadowing x
-    
+
     {
         let x = x * 2;
         println!("the value of x in inner scope is: {x}");
@@ -37,7 +36,6 @@ fn main() {
     // let mut spaces = "    ";
     // spaces = spaces.len();
 
-
     let mut y = 1;
     {
         let mut y = y + 1;
@@ -45,4 +43,47 @@ fn main() {
         println!("inner scope {y}");
     }
     println!("{y}");
+
+
+    // Scalar Types
+    let value1: i8 = 1; // u8 for unsigned
+    let value2: i16 = 1; // u16 for unsigned
+    let value3: i32 = 1; // u32 for unsigned
+    let value4: i64 = 1; // u64 for unsigned
+    let value5: i128 = 1; // u128 for unsigned
+    let value6: isize = 1; // based on os architecture 32 or 64, usize for unsigned
+
+    let float1: f32 = 2.0;
+    let float2: f64 = 2.0;
+
+    let c1 = 'z';
+    let c2: char = 'A';
+    let heart_eyed_cat = '😻';
+    // let c2 = "z"; it is string not character
+    // let c: char = "z"; ERROR!
+
+    // number literal 
+    let decimal_literal = 10_000;
+    let hex_literal = 0xff;
+    let octal_literal = 0o77;
+    let binary_literal = 0b10;
+    let byte_literal = b'A';
+
+
+    // => Compound Types (Tuple, Array)
+    
+    // Tuple
+    // The tuple without any values has a special name, unit. 
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (n, o, p) = tup; // destructuring
+    println!("value of o is {o}");
+    let third_element = tup.2;
+    println!("third element of tup is {third_element}");
+
+    
+    // Array
+    let array = [1, 2, 3, 4, 5];
+    let arr = [5; 10];
+    let first = arr[0];
+    println!("first is {first}");
 }
